6b52625c07f9255a8675072574a1b80c6f8910e7
Raj <rajshah0192000@gmail.com>
2023-12-27 15:34:28 -0500

    Completed Skills Page

--- src/data/experience/ExperienceInformation.js
+++ src/data/experience/ExperienceInformation.js
@@ -3,24 +3,24 @@ export const data = [
     companyName: "Empowered Futures Canada",
     period: "Nov 2023 - Present",
     description:
-      "Worked on slowly migrating a legacy JSP application to React in a microfrontend architecture where only part of the system is React withTypescript and the rest remains legacy JSP interface",
+      "Currently working as a volunteering front-end developer with Empowered Futures, contributing to two projects. One involves developing an internal application, while the other focuses on creating a mentorship platform.",
   },
   {
     companyName: "Accenture India",
     period: "Sep 2021 - Aug 2023",
     description:
-      "Worked on slowly migrating a legacy JSP application to React in a microfrontend architecture where only part of the system is React withTypescript and the rest remains legacy JSP interface",
+      "Developed and maintained multiple applications of Ireland banking domain, while working as a part of the JavaScript team. Implemented new functionalities in applications and provided support for existing applications. Utilized a range of JavaScript frameworks such as ReactJs, NodeJs, ExpressJs, and MongoDB.",
   },
   {
     companyName: "Synoverge Technologies Pvt. Ltd.",
     period: "Feb 2021 - Sep 2021",
     description:
-      "Worked on slowly migrating a legacy JSP application to React in a microfrontend architecture where only part of the system is React withTypescript and the rest remains legacy JSP interface",
+      "Collaborated with a team on multiple projects to design and develop software solutions addressing real-world problems. Actively participated in Agile software development, contributing to both iOS and Android projects. Engaged in diverse projects involving Dart programming, mobile application development, web development. Developed and maintained mobile and web applications using Flutter.",
   },
   {
     companyName: "AHAsolar Technologies Ltd.",
     period: "May 2020 - Jan 2021",
     description:
-      "Worked on slowly migrating a legacy JSP application to React in a microfrontend architecture where only part of the system is React withTypescript and the rest remains legacy JSP interface",
+      "During my internship, I took on the responsibility of learning about logic development with Python and the basics of Optical Character Recognition (OCR). Collaborated with my supervisor and researched online resources to gain a deeper understanding of OCR functionality.",
   },
 ];
