1ba5536c6b39f0c4876e58066caaa2b92762d531
Raj <rajshah0192000@gmail.com>
2024-01-01 15:58:20 -0500

    Fixed bug in the skill page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -8,7 +8,7 @@ function Skills() {
       <h1 className=" text-websiteColor underline underline-offset-[16px] text-4xl	font-medium">
         Skills
       </h1>
-      <div className="mt-14 flex justify-between self-center h-screen">
+      <div className="mt-14 flex justify-between items-center self-center h-screen">
         <div className="h-[32rem] grid grid-cols-3 md:grid-cols-6 lg:grid-cols-8 gap-x-8">
           {skillData.map((skill) => (
             <SkillsDetails skill={skill} key={skill.skillName} />
