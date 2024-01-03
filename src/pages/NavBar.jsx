0e747ef0ab70a57bacc830a5838d114bd2760e4b
Raj <rajshah0192000@gmail.com>
2024-01-02 19:13:05 -0500

    Fixed Navbar Bug

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -4,7 +4,7 @@ import NavList from "../ui/NavList";
 function NavBar() {
   return (
     <nav className="sticky w-full top-0 z-20 backdrop-filter backdrop-blur-sm opacity-90 ">
-      <div className="flex flex-wrap justify-between mx-auto p-4 items-center">
+      <div className="flex flex-wrap justify-between mx-auto p-5 items-center">
         <Logo />
         <NavList />
       </div>
